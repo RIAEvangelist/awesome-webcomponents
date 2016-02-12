ee6fbce69e6ec2bc605261531ee45aba7fdafcef
Brandon Nozaki Miller <brandon@diginow.it>
2016-02-11 22:49:32 -0800

    added buttonset

---
+++ components/buttons/awesome-buttonset.js
@@ -0,0 +1,54 @@
+'use strict';
+
+awesome.requireCSS(`${awesome.path}components/buttons/awesome-buttonset.css`);
+
+(
+    function(){
+        const defaults={
+            count:3,
+            'b0':0,
+            'b1':1,
+            'b2':2
+        }
+
+        class Component extends HTMLElement{
+            createdCallback(){
+                awesome.mergeDataset(this,defaults);
+
+                const count=Number(this.dataset.count);
+                const buttons=[];
+
+                for(let i=0; i<count; i++){
+                    buttons.push(
+                        `<button>${
+                            this.dataset[
+                                `b${i}`
+                            ]
+                        }</button>`
+                    )
+                }
+
+                this.innerHTML=`
+                    ${buttons.join('')}
+                `;
+            }
+
+            attachedCallback(){
+
+            }
+
+            detachedCallback(){
+
+            }
+
+            attributeChangedCallback(key,oldValue,newValue){
+
+            }
+        }
+
+        document.registerElement(
+            'awesome-buttonset',
+            Component
+        );
+    }
+)();
