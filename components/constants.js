038399c8d0c2c849214324e4f297c59515ce0889
Brandon Nozaki Miller <brandon@diginow.it>
2016-02-12 01:13:54 -0800

    fixed login

--- components/constants.js
+++ components/constants.js
@@ -7,7 +7,7 @@ awesome.constants.component={
     DRAG_DROP_LEAVE:'dragdrop.leave',
 
     //validators
-    VALIDATE_USERNAME:'[a-zA-Z0-9\\@\\.\\-\\_]',
+    VALIDATE_USERNAME:'[a-zA-Z0-9\\@\\.\\-\\_]{6,30}',
     VALIDATE_EMAIL:'([a-zA-Z0-9\.\-\_]{1,20})\@([a-zA-Z0-9\.\-\_]{2,50})\.([a-zA-Z]{2,10})',
     VALIDATE_URL:'^(https?:\/\/)?([\da-z\.-]+)\.(.{2,100})$'
 };
--- screens/login/awesome-login-screen.js
+++ screens/login/awesome-login-screen.js
@@ -60,7 +60,7 @@ awesome.requireScript(`${awesome.path}components/dialog/awesome-dialog.js`);
                                 value=''
                                 required='true'
                                 minlength=6
-                                maxlength=20
+                                maxlength=30
                                 pattern='${this.dataset.username_pattern}'
                                 class=''
                                 placeholder='${this.dataset.username_placeholder}'
@@ -69,6 +69,7 @@ awesome.requireScript(`${awesome.path}components/dialog/awesome-dialog.js`);
                                 id='${this.dataset.password_id}'
                                 value=''
                                 required='true'
+                                type='password'
                                 placeholder='${this.dataset.password_placeholder}'
                             />
                             <div class='button-wrapper'>
