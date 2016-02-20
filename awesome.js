84a6afda5393adcc1ebe6bd8bf8401f7d55171b9
Brandon Nozaki Miller <brandon@diginow.it>
2016-02-20 03:06:12 -0800

    IE Array.prototype.includes polyfill

--- awesome.js
+++ awesome.js
@@ -295,3 +295,40 @@ if(!document.registerElement){
         `
     );
 }
+
+if (!Array.prototype.includes) {
+    Array.prototype.includes = function(searchElement) {
+        const O = Object(this);
+        const len = parseInt(O.length,10) || 0;
+
+        if (len === 0) {
+            return false;
+        }
+
+        const n = parseInt(arguments[1],10) || 0;
+        let k;
+
+        if (n >= 0) {
+            k = n;
+        } else {
+            k = len + n;
+            if (k < 0) {
+                k = 0;
+            }
+        }
+
+        let currentElement;
+
+        while (k < len) {
+            currentElement = O[k];
+            if (
+                searchElement === currentElement
+                || (searchElement !== searchElement && currentElement !== currentElement) //NaN !== NaN
+            ){
+                return true;
+            }
+            k++;
+        }
+        return false;
+    };
+}
