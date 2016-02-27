6bc410b47ab32f6940303e49297837439e3a7c16
Mario Solorzano <marioxcore1@gmail.com>
2016-02-26 16:54:46 -0800

    updated video controls

--- components/video/awesome-vimeo.js
+++ components/video/awesome-vimeo.js
@@ -5,16 +5,35 @@ awesome.requireCSS(`${awesome.path}components/video/awesome-vimeo.css`);
 (
     function(){
         const defaults={
-            source:'',
+            video_id:'',
+            allow_fullscreen: true,
+            autopause: 1,
+            autoplay: 0,
+            badge: 0,
+            byline: 0,
+            color: '00adef',
+            loop: 0,
+            protrait: 0,
+            title:1
         }
 
         class Component extends HTMLElement{
             createdCallback(){
                 awesome.mergeDataset(this,defaults);
 
+                let vimeoURL = `https://player.vimeo.com/video/${this.dataset.video_id}?`;
+                for(const i in this.dataset){
+                    if(i == 'video_id' || i == 'allow_fullscreen'){
+                        continue;
+                    }
+
+                    vimeoURL = vimeoURL.concat(`${i}=${this.dataset[i]}&`);
+                }
+
                 this.innerHTML=`
-                    <iframe width="420" height="315"
-                        src = "http://player.vimeo.com/video/${this.dataset.source}"
+                    <iframe
+                        src = '${vimeoURL}'
+                        allowfullscreen = ${this.dataset.allow_fullscreen}
                     ></iframe>
                 `;
             }
--- components/video/awesome-youtube.js
+++ components/video/awesome-youtube.js
@@ -4,19 +4,49 @@ awesome.requireCSS(`${awesome.path}components/video/awesome-youtube.css`);
 
 (
     function(){
+        //full documentation of these parameters can be seen @
+        //https://developers.google.com/youtube/player_parameters#controls
         const defaults={
-            source:'',
+            video_id:'',
+            allow_fullscreen: true,
+            autoplay: 0,
+            controls: 1,
+            color: 'white',
+            disablekb: 1,
+            enablejsapi: 1,
+            end: -1,
+            fs: 1,
+            h1: 'eng',
+            iv_load_policy: 3,
+            modestbranding: 1,
+            loop: 0,
+            rel: 0,
+            showinfo: 0,
+            start: -1,
+            theme: 'dark'
         }
 
         class Component extends HTMLElement{
+
             createdCallback(){
                 awesome.mergeDataset(this,defaults);
 
+                let youTubeURL = `https://www.youtube.com/embed/${this.dataset.video_id}?`;
+                for(const i in this.dataset){
+                    if(i == 'video_id' || i == 'allow_fullscreen'){
+                        continue;
+                    }
+
+                    youTubeURL = youTubeURL.concat(`${i}=${this.dataset[i]}&`);
+                }
+
                 this.innerHTML=`
-                    <iframe width="420" height="315"
-                        src = "http://www.youtube.com/embed/${this.dataset.source}"
+                    <iframe
+                        src = '${youTubeURL}'
+                        allowfullscreen = ${this.dataset.allow_fullscreen}
                     ></iframe>
                 `;
+
             }
 
             attachedCallback(){
