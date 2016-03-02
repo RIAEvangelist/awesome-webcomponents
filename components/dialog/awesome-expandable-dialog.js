b1193b8e4a5001281c0d5ad844fd4ef52c8ef3d6
Adrian Sida <asida@2-sight.com>
2016-03-01 20:31:25 -0800

    PR comments addressed. component name changes and adding header.

--- components/dialog/awesome-expandable-dialog.js
+++ components/dialog/awesome-expandable-dialog.js
@@ -1,21 +1,36 @@
 'use strict';
 
-awesome.requireCSS(`${awesome.path}components/document/awesome-document-viewer.css`);
+awesome.requireCSS(`${awesome.path}components/dialog/awesome-expandable-dialog.css`);
 awesome.requireScript(`${awesome.path}components/dialog/awesome-dialog.js`);
+awesome.requireScript(`${awesome.path}components/header/awesome-header.js`);
 
 (
     function(){
-        const defaults={};
+        const defaults={
+            icon:'',
+            title:'',
+            header:true
+        };
 
         class Component extends HTMLElement{
             createdCallback(){
                 awesome.mergeDataset(this,defaults);
                 const content=awesome.loadTemplate(this);
 
+                let header='';
+
+                if(this.dataset.header==='true'){
+                    header=`
+                        <awesome-header
+                            data-icon='${this.dataset.icon}'
+                            data-title='${this.dataset.title}'
+                        ></awesome-header>
+                    `;
+                }
+
                 this.innerHTML=`
-                    <awesome-dialog
-                        data-header='false'
-                    >
+                    <awesome-dialog>
+                        ${header}
                         <div class='content'>
                             ${content}
                         </div>
@@ -49,7 +64,7 @@ awesome.requireScript(`${awesome.path}components/dialog/awesome-dialog.js`);
         }
 
         document.registerElement(
-            'awesome-document-viewer',
+            'awesome-expandable-dialog',
             Component
         );
     }
