77343aa41f9de1751abf00caaca7059ef248b30c
Mario Solorzano <marioxcore1@gmail.com>
2016-03-03 23:20:51 -0800

    added notification modal and added example

---
+++ components/modal/awesome-notification-modal.js
@@ -0,0 +1,66 @@
+'use strict';
+
+awesome.requireCSS(`${awesome.path}components/modal/awesome-notification-modal.css`);
+awesome.requireScript(`${awesome.path}components/modal/awesome-modal.js`);
+
+(
+    function(){
+        const defaults={
+            title: '',
+            content: '',
+        };
+
+        class Component extends HTMLElement{
+            createdCallback(){
+                awesome.mergeDataset(this,defaults);
+                const content=awesome.loadTemplate(this);
+
+                this.innerHTML=`
+                    <awesome-modal>
+                        <template>
+                            <h1>
+                                ${this.dataset.title}
+                            </h1>
+                            <p>
+                                ${this.dataset.content}
+                            </p>
+                            <button class = 'closeButton'>
+                                OK
+                            </button>
+                        </template>
+                    </awesome-modal>
+                `;
+            }
+
+            attachedCallback(){
+                this.addEventListener(
+                    'click',
+                    this.click.bind(this)
+                );
+            }
+
+            detachedCallback(){
+
+            }
+
+            attributeChangedCallback(key,oldValue,newValue){
+                this.createdCallback();
+            }
+
+            click(e){
+                if(e.target.classList.contains('closeButton')){
+                    this.querySelector('awesome-modal').close();
+                }
+            }
+
+            open(){
+                this.querySelector('awesome-modal').open();
+            }
+
+        }
+        document.registerElement(
+            'awesome-notification-modal',
+            Component
+        );
+    }
+)();
