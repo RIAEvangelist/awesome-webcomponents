7ca2a5d2c61ab144825a9076c5b2140eafe4b83c
Mario Solorzano <marioxcore1@gmail.com>
2016-03-23 15:56:54 -0700

    removed hardcoded screenList name

--- screens/screenList/awesome-screen-list.js
+++ screens/screenList/awesome-screen-list.js
@@ -10,7 +10,6 @@ awesome.requireCSS(`${awesome.path}screens/screenList/awesome-screen-list.css`);
         let constants = null;
         let action = null;
         let defaults={
-            screen:'screenList'
         };
 
         function init(e){
