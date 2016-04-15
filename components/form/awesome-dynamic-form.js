d71d6fa23074c05e362903616c2a10949c8d5771
Mario Solorzano <marioxcore1@gmail.com>
2016-04-15 14:13:36 -0700

    initialized form for development

---
+++ components/form/awesome-dynamic-form.js
@@ -0,0 +1,74 @@
+'use strict';
+
+awesome.requireCSS(`${awesome.path}components/form/awesome-dynamic-form.css`);
+// awesome.requireCSS(`${awesome.path}stores/_a_boilerplate/boilerplate.js`);
+
+(
+    function(){
+
+        const defaults={
+
+        }
+
+
+        function init(e){
+            window.off(
+                'awesome-ready',
+                init
+            );
+
+            dispatcher=awesome.dispatchers.component;
+            constants=awesome.constants.component;
+            action=awesome.constants.action;
+
+            // store=awesome.store.boilerplate;
+
+            document.registerElement(
+                'awesome-dynamic-form',
+                Component
+            );
+        }
+
+        class Component extends HTMLElement{
+            createdCallback(){
+                awesome.mergeDataset(this,defaults);
+
+                this.innerHTML=`
+
+                `;
+            }
+
+            attachedCallback(){
+                // this.store.on(
+                //     'change',
+                //     update.bind(this)
+                // )
+            }
+
+            detachedCallback(){
+
+            }
+
+            attributeChangedCallback(key,oldValue,newValue){
+                //just re-render for this simple example
+                this.createdCallback();
+            }
+
+            // update(){
+            //     //just re render this simple example
+            //     this.createdCallback();
+            // }
+        }
+
+        if(!awesome.ready){
+            window.on(
+                'awesome-ready',
+                init
+            );
+
+            return;
+        }
+
+        init();
+    }
+)();
