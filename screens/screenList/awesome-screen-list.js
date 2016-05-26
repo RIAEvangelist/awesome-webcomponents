67ea657718b977253fb382c91ef1ec1ae0128984
Adrian Sida <asida@2-sight.com>
2016-05-26 14:51:44 -0700

    Modified awesome-screen-list to new format and added localization handling of awesome-screen-icon text.

--- screens/screenList/awesome-screen-list.js
+++ screens/screenList/awesome-screen-list.js
@@ -1,26 +1,34 @@
 'use strict';
 
+awesome.requireScript(`${awesome.path}stores/router/route.js`);
 awesome.requireCSS(`${awesome.path}screens/screenList/awesome-screen-list.css`);
-
+awesome.requireScript(`${awesome.path}components/icons/awesome-screen-icon.js`);
 
 (
     function(){
-        let state=null;
+        let route=null;
         let dispatcher=null;
         let constants = null;
         let action = null;
-        let defaults={
+
+        const caresAbout=[];
+        const defaults={
+            screen:'app-list',
+            icon:'flaticon-show-apps-button',
+            link_text:'Home',
+            show_all:true
         };
 
         function init(e){
-            dispatcher=awesome.dispatchers.component;
-            constants = awesome.constants.component;
-            action = awesome.constants.action;
-
             window.off(
                 'awesome-ready',
                 init
             );
+
+            route=awesome.stores.route.state;
+            dispatcher=awesome.dispatchers.component;
+            constants = awesome.constants.component;
+            action = awesome.constants.action;
 
             document.registerElement(
                 'awesome-screen-list',
@@ -31,17 +39,48 @@ awesome.requireCSS(`${awesome.path}screens/screenList/awesome-screen-list.css`);
         class Component extends HTMLElement{
             createdCallback(){
                 awesome.mergeDataset(this,defaults);
-                const content=awesome.loadTemplate(this);
+
+                if(!this.screens && !this.dataset.show_all){
+                    return;
+                }
+
+                if(route.screens.length<2){
+                    return;
+                }
+
+                let content='';
+
+                for (let i = 0; i < route.screens.length; i++) {
+                    const screen = route.screens[i];
+                    console.log(screen.dataset.screen)
+                    if(
+                        (
+                            this.screens
+                            && !this.screens.includes(screen.dataset.screen)
+                        )||this.dataset.screen===screen.dataset.screen
+                    ){
+                        continue;
+                    }
+                    content = `
+                        ${content}
+                        <awesome-screen-icon
+                            ${
+                                (!screen.dataset.icon.includes('.'))
+                                ? `data-class= '${screen.dataset.icon}'`
+                                : `data-icon = '${screen.dataset.icon}'`
+                            }
+                            data-text= '${screen.dataset.link_text}'
+                            data-screen_name= '${screen.dataset.screen}'
+                        >
+                        </awesome-screen-icon>
+                    `;
+                }
 
                 this.innerHTML=`
                     <div class='screensList-container'>
-                        ${content.content}
+                        ${content}
                     </div>
-                    ${content.template}
                 `;
-
-                const appCount=this.querySelector('awesome-screen-icon').length;
-                //do something
             }
 
             attachedCallback(){
@@ -50,42 +89,49 @@ awesome.requireCSS(`${awesome.path}screens/screenList/awesome-screen-list.css`);
                     this.clicked
                 );
 
-                window.addEventListener(
-                    'resize',
-                    this.resize.bind(this)
-                );
-
-                setTimeout(
-                    function(){
-                        const container=this.querySelector('.screensList-container');
-                        container.style.top=`calc(50% - ${container.offsetHeight/2}px)`;
-                    }.bind(this),
-                    1
+                this.screenCount=route.screens.length;
+                route.on(
+                    'change',
+                    this.checkScreens.bind(this)
                 );
             }
 
             detachedCallback(){
+                route.off(
+                    'change',
+                    this.checkScreens.bind(this)
+                );
+            }
 
+            checkScreens(){
+                if(this.screenCount==route.screens.length){
+                    return;
+                }
+                this.screenCount=route.screens.length;
+                console.log(this.screenCount,route.screens.length)
+                this.createdCallback();
             }
 
             attributeChangedCallback(key,oldValue,newValue){
-                if(key==='style'){
+                if(!caresAbout.includes(key)){
                     return;
                 }
                 this.createdCallback();
             }
 
+            load(screens){
+                if(!screens){
+                    return;
+                }
+                this.screens = screens;
+                this.createdCallback();
+            }
 
             clicked(e){
                 dispatcher.trigger(
                     action.ROUTE_REQUEST,
                     e.target.dataset.screen_name
                 );
-            }
-
-            resize(e){
-                const container=this.querySelector('.screensList-container');
-                container.style.top=`calc(50% - ${container.offsetHeight/2}px)`;
             }
         }
 
