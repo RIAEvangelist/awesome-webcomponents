1e13afef4adc966142dd92fe4b53532767eb92c6
Brandon Nozaki Miller <brandon@diginow.it>
2016-06-15 01:35:46 -0700

    updated boilerplate

--- components/_a_boilerplate/awesome-boilerplate-advanced.js
+++ components/_a_boilerplate/awesome-boilerplate-advanced.js
@@ -3,81 +3,45 @@
 awesome.requireCSS(`${awesome.path}components/_a_boilerplate/awesome-boilerplate.css`);
 awesome.requireCSS(`${awesome.path}stores/_a_boilerplate/boilerplate.js`);
 
+
+'use strict';
+
+awesome.requireCSS(`${awesome.path}components/dialog/awesome-dialog.css`);
+awesome.requireScript(`${awesome.path}components/header/awesome-header.js`);
+
 (
     function(){
-        let store=null;
-        let dispatcher=null;
-        let constants = null;
-        let action = null;
         const defaults={
             something:'Boilerplate'
         }
 
+        const component=new AwesomeComponent;
+        component.tagName='awesome-boilerplate-example';
 
-        function init(e){
-            window.off(
-                'awesome-ready',
-                init
-            );
+        component.create=function createAwesomeDialog() {
+            const dispatcher=awesome.dispatchers.component;
+            const constants=awesome.constants.component;
+            const action=awesome.constants.action;
 
-            dispatcher=awesome.dispatchers.component;
-            constants=awesome.constants.component;
-            action=awesome.constants.action;
+            const store=awesome.store.boilerplate;
 
-            store=awesome.store.boilerplate;
+            return class Component extends HTMLElement{
+                createdCallback(){
+                    super.createdCallback();
+                    this.mergeDataset(this,defaults);
 
-            document.registerElement(
-                'awesome-boilerplate-advanced',
-                Component
-            );
-        }
+                    this.innerHTML=`
+                        <p>${this.dataset.something}</p>
+                        <p>store.state.boilerplate=${store.state.boilerplate}</p>
+                        <div>${this.content.content}</div>
 
-        class Component extends HTMLElement{
-            createdCallback(){
-                awesome.mergeDataset(this,defaults);
-                const content=awesome.loadTemplate(this);
-
-                this.innerHTML=`
-                    <p>${this.dataset.something}</p>
-                    <p>store.state.boilerplate=${store.state.boilerplate}</p>
-                    <div>${content.content}</div>
-
-                    <!-- preserve content template so it isn't lost on re-render -->
-                    ${content.template}
-                `;
-            }
-
-            attachedCallback(){
-                this.store.on(
-                    'change',
-                    update.bind(this)
-                )
-            }
-
-            detachedCallback(){
-
-            }
-
-            attributeChangedCallback(key,oldValue,newValue){
-                //just re-render for this simple example
-                this.createdCallback();
-            }
-
-            update(){
-                //just re render this simple example
-                this.createdCallback();
+                        <!-- preserve content template so it isn't lost on re-render -->
+                        ${this.content.template}
+                    `;
+                }
             }
         }
 
-        if(!awesome.ready){
-            window.on(
-                'awesome-ready',
-                init
-            );
-
-            return;
-        }
-
-        init();
+        component.init();
     }
 )();
