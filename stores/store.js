7bec22d65a09b4a8e35d04392b32524410be1352
Brandon Nozaki Miller <brandon@diginow.it>
2016-06-23 07:22:50 -0700

    event scoping

--- stores/store.js
+++ stores/store.js
@@ -21,7 +21,7 @@ awesome.requireScript(`${awesome.bower}js-message/js-message-vanilla.js`);
         class Store{
             constructor(){
                 const events=new pubsub;
-                this._events=events;
+                this._events=events.bind(events);
                 Object.defineProperties(
                     this,
                     {
